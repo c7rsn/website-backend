f1ff435d0fde085555e9ce37f2a9bfeaff48c39e
Carson <carsonredacted@gmail.com>
2024-10-07 10:48:44 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    socket.on('chatMessage', (msg) => {
+        io.emit('message', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
